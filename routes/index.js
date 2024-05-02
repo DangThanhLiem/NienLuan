e7add3c8f7fa929fd4fb8bb957b2dea23fb3154b
B2014580_DangThanhLiem <liemb2014580@student.ctu.edu.vn>
2024-05-03 02:11:52 +0700

    Backend done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const userRouter = require('./user')
 const productRouter = require('./product')
 const orderRouter = require('./order')
-const customerRouter = require('./customer')
 const categoryRouter =require('./category')
 const couponRouter =require('./coupon')
 const {notFound,errHandler} = require('../middlewares/errHandler')
@@ -13,7 +12,6 @@ const initRouters = (app) =>{
     app.use('/api/category',categoryRouter)
     app.use('/api/order',orderRouter)
     app.use('/api/coupon',couponRouter)
-    app.use('/api/customer',customerRouter)
 
 
     app.use(notFound)
